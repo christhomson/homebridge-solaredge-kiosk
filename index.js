32bd72240a1e84a290c8227529a4a80668bc0bef
Chris Thomson <chris@cthomson.ca>
2020-07-18 14:32:29 -0400

    Spacing changes

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ const setupCache = require('axios-cache-adapter').setupCache;
 var Service, Characteristic;
 
 const DEF_MIN_LUX = 0,
-      DEF_MAX_LUX = 10000;
+  DEF_MAX_LUX = 10000;
 
 const PLUGIN_NAME   = 'homebridge-solaredge-inverter';
 const ACCESSORY_NAME = 'SolarEdge Inverter';
 
 module.exports = function(homebridge) {
-    Service = homebridge.hap.Service;
-    Characteristic = homebridge.hap.Characteristic;
-    homebridge.registerAccessory(PLUGIN_NAME, ACCESSORY_NAME, SolarEdgeInverter);
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerAccessory(PLUGIN_NAME, ACCESSORY_NAME, SolarEdgeInverter);
 }
 
 /**
  * Setup Cache For Axios to prevent additional requests
- */
+*/
 const cache = setupCache({
   maxAge: 5 * 1000 //in ms
 })
@@ -33,12 +33,12 @@ const api = axios.create({
  * @param {apiKey} the SolarEdge monitoring API Key for access to the Site
  */
 const getInverterData = async(siteID, apiKey) => {
-	try {
-//	    return await api.get('https://'+inverterIp+'/solar_api/v1/GetPowerFlowRealtimeData.fcgi')
-	    return await api.post(`https://monitoringpublic.solaredge.com/solaredge-web/p/kiosk/kioskData?locale=en_US&guid=${apiKey}`);
-	} catch (error) {
-	    console.error(error)
-	}
+  try {
+    //	    return await api.get('https://'+inverterIp+'/solar_api/v1/GetPowerFlowRealtimeData.fcgi')
+    return await api.post(`https://monitoringpublic.solaredge.com/solaredge-web/p/kiosk/kioskData?locale=en_US&guid=${apiKey}`);
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 /**
@@ -51,55 +51,55 @@ const getInverterData = async(siteID, apiKey) => {
  */
 const getAccessoryValue = async (siteID, apiKey, log) => {
 
-	// To Do: Need to handle if no connection
-	const inverterData = await getInverterData(siteID, apiKey)
+  // To Do: Need to handle if no connection
+  const inverterData = await getInverterData(siteID, apiKey)
 
-	if(inverterData) {
-		log.info('Data from API', inverterData.data);
-		const systemPower = inverterData.data.match(/systemPower:"(\d+)/)[1];
-		const currentPower = inverterData.data.match(/currentPower:"(\d+\.?\d*)/)[1];
+  if(inverterData) {
+    log.info('Data from API', inverterData.data);
+    const systemPower = inverterData.data.match(/systemPower:"(\d+)/)[1];
+    const currentPower = inverterData.data.match(/currentPower:"(\d+\.?\d*)/)[1];
 
-		log.info(`system power = ${systemPower}, current power = ${currentPower}`);
-	        return parseFloat(currentPower);
-	} else {
-		// No response inverterData return 0
-		log.info('No valid API response.');
-		return 0
-	}
+    log.info(`system power = ${systemPower}, current power = ${currentPower}`);
+    return parseFloat(currentPower);
+  } else {
+    // No response inverterData return 0
+    log.info('No valid API response.');
+    return 0
+  }
 }
 
 class SolarEdgeInverter {
-    constructor(log, config) {
-    	this.log = log
-    	this.config = config
+  constructor(log, config) {
+    this.log = log
+    this.config = config
 
-    	this.service = new Service.LightSensor(this.config.name)
+    this.service = new Service.LightSensor(this.config.name)
 
-    	this.name = config["name"];
-    	this.manufacturer = config["manufacturer"] || "SolarEdge";
-	    this.model = config["model"] || "Inverter";
-	    this.serial = config["serial"] || "solaredge-inverter-1";
-	    this.site_id = config["site_id"];
-	    this.api_key = config["api_key"];
-	    this.minLux = config["min_lux"] || DEF_MIN_LUX;
-    	this.maxLux = config["max_lux"] || DEF_MAX_LUX;
-    }
+    this.name = config["name"];
+    this.manufacturer = config["manufacturer"] || "SolarEdge";
+    this.model = config["model"] || "Inverter";
+    this.serial = config["serial"] || "solaredge-inverter-1";
+    this.site_id = config["site_id"];
+    this.api_key = config["api_key"];
+    this.minLux = config["min_lux"] || DEF_MIN_LUX;
+    this.maxLux = config["max_lux"] || DEF_MAX_LUX;
+  }
 
-    getServices () {
-    	const informationService = new Service.AccessoryInformation()
-        .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
-        .setCharacteristic(Characteristic.Model, this.model)
-        .setCharacteristic(Characteristic.SerialNumber, this.serial)
+  getServices () {
+    const informationService = new Service.AccessoryInformation()
+      .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
+      .setCharacteristic(Characteristic.Model, this.model)
+      .setCharacteristic(Characteristic.SerialNumber, this.serial)
 
-        this.service.getCharacteristic(Characteristic.CurrentAmbientLightLevel)
-	      .on('get', this.getOnCharacteristicHandler.bind(this))
+    this.service.getCharacteristic(Characteristic.CurrentAmbientLightLevel)
+      .on('get', this.getOnCharacteristicHandler.bind(this))
 
-	    return [informationService, this.service]
-    }
+    return [informationService, this.service]
+  }
 
-    async getOnCharacteristicHandler (callback) {
-	    this.log(`calling getOnCharacteristicHandler`, await getAccessoryValue(this.site_id, this.api_key, this.log))
+  async getOnCharacteristicHandler (callback) {
+    this.log(`calling getOnCharacteristicHandler`, await getAccessoryValue(this.site_id, this.api_key, this.log))
 
-	    callback(null, await getAccessoryValue(this.site_id, this.api_key, this.log))
-	}
+    callback(null, await getAccessoryValue(this.site_id, this.api_key, this.log))
+  }
 }
